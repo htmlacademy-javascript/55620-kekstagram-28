8d02b6c87083a595b7684e0fc83c9448f0d7cf0e
Polina Emelyanova <polina_emelyanov@mail.ru>
2023-02-19 14:02:04 +0700

    dev: polindromeChecker function

--- js/functions.js
+++ js/functions.js
@@ -10,3 +10,27 @@ function checkStringLength(str, length) {
 
 checkStringLength('проверяемая строка 1', 20);
 checkStringLength('проверяемая строка', 10);
+
+//Функция для проверки, является ли строка палиндромом
+/*
+@example polindromeChecker('топот'); // Результат: true - строка является палиндромом
+@example polindromeChecker('Кекс');  // Результат: false - это не палиндром
+*/
+
+function polindromeChecker(str) {
+  const originalStr = str.trim().toLowerCase().split('').reverse();
+  const baseStr = str.trim().toLowerCase().split(' ').join('');
+  let resultStr = '';
+  for (let i = 0; i < originalStr.length; i++) {
+    if (originalStr[i] !== ' ') {
+      resultStr += originalStr[i];
+    }
+  }
+
+  return (resultStr === baseStr);
+}
+
+polindromeChecker('Лёша на полке клопа нашёл ');
+polindromeChecker('А роза упала на лапу Азора');
+polindromeChecker('Кекс');
+polindromeChecker('ДовОд');
